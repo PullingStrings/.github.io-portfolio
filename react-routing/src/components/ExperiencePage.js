2bfb4c4e357c4e90863aa463beaa53b7848a44d3
PullingStrings <drammatik@gmail.com>
2017-11-15 17:01:43 +0000

    site

--- react-routing/src/components/ExperiencePage.js
+++ react-routing/src/components/ExperiencePage.js
@@ -5,48 +5,23 @@ class ExperiencePage extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1>Experiences</h1>
+        <h1>Experience</h1>
         <div className="ExContainer">
+          <h2>Web Development at General Assembly</h2>
+          <h3>July 2017 - Nov 2017</h3>
           <p>An intensive three-month full-time programme, teaching the fundamentals of
-Web Development through practical application delivered by industry
-experts.
-
-In the three months, our responsibilities and assignments were to create code and deploy 4 web apps.
-
-Project-4
-Play-Gen Finder
-A web app that lets you find playlists from people around you.
-https://playgenfinder.herokuapp.com/
-
-Built with: ReactJS, Express, Node.js, JavaScript, MongoDB, Mongoose
-HTML5, SCSS, WEBpack
-This app allows you to find users in any area you are in and lets you follow their playlists built using React JS and Express as the backend, you will be able to log in using Spotify and find music anywhere you are.
-
-Project-3
-Lost & Found pets
-A Web app that allows you to find your pet.
-https://lost-and-found-pets.herokuapp.com/
-
-Built with: AngularJS, Express, Node.js, JavaScript, MongoDB, Mongoose
-HTML5, SCSS, Gulp and Bower
-
-This was a group project built to help pet owners to locate and find their lost pets. I did most of the styling for the whole app with advice from my team members using CSS and AngularJS.
-
-Project-2
-Learn to dance mzansi
-A fun web app that teaches you how to dance on different styles of music from all around the world starting with Southern Africa.
-https://learndancemzansi.herokuapp.com/
-
-Built with: HTML5, Bulma BootStrap, JavaScript, ES6, jQuery 3.10, Mongoose
-Node, SCSS, Gulp and Yarn
-
-This was an individual project. I have used EJS together with jQuery and Ajax as a front-end tool to create a very slick looking web app that teaches you how to dance. Fully RESTful you can log in and log out securely and upload videos that are kept in the database that I created from scratch using MongoDB and Express.
-
-Project-1
-Pop The Balloons
-A simple exciting game where you get to pop some balloons on your way home.
-https://pop-the-balloons.herokuapp.com/
-This was the first time I fully built something from ground up just using CSS and HTML.</p>
+            Web Development through practical application delivered by industry
+            experts
+          </p>
+          <p>In the three months, our responsibilities and assignments were to create code and deploy 4 web apps:
+            <ul>
+              <li>NodeJs</li>
+              <li>Express</li>
+              <li>JavaScript</li>
+              <li>React</li>
+              <li>MongoDB</li>
+            </ul>
+          </p>
         </div>
       </div>
     );
